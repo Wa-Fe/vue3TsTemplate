b412eae17850c424b2f972a03b5c2b3ecb9c8b67
long <wa.fe@foxmail.com>
2025-02-20 18:27:42 +0800

    feat: 暂存

---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss"></style>
